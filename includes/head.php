c5726b72bb21cfadd72b44da0649cd5ada0c1a23
Standard IP <rhroyston@gmail.com>
2015-09-16 20:35:57 +0000

    My changes

--- includes/head.php
+++ includes/head.php
@@ -12,6 +12,7 @@
 
 <!-- Style sheet link -->
 <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+<link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Bangers|Special+Elite' type='text/css'>
 <link rel="stylesheet" href="https://storage.googleapis.com/code.getmdl.io/1.0.0/material.min.css">
 <link rel="stylesheet" href="https://storage.googleapis.com/code.getmdl.io/1.0.4/material.grey-deep_orange.min.css" />
 <link rel="stylesheet" href="css/base.css">
