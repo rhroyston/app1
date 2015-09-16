6f0726fb6eb04d02f04ce2548459746019121685
Standard IP <rhroyston@gmail.com>
2015-09-16 19:16:32 +0000

    My changes

--- includes/registrationcard.php
+++ includes/registrationcard.php
@@ -5,52 +5,62 @@
     <div class="mdl-card__supporting-text mdl-grid">
         <form method="post" action="includes/reg">
             <div class="mdl-textfield mdl-js-textfield mdl-cell--6-col">    
-                <input class="mdl-textfield__input" type="text" minlength="3" name="username" id="username" value="" placeholder="Username...">
+                <input class="mdl-textfield__input" type="text" minlength="3" name="username" id="username" value="">
                 <span class="mdl-textfield__error">Minimum Length 3 Characters</span>
+                <label class="mdl-textfield__label" for="sample1">Username...</label>
             </div>
             <div class="mdl-textfield mdl-js-textfield mdl-cell--6-col">
             </div>
             <div class="mdl-textfield mdl-js-textfield mdl-cell--6-col">
-                <input class="mdl-textfield__input" type="password" minlength="5" name="password" id="password" value="" placeholder="Password...">
+                <input class="mdl-textfield__input" type="password" minlength="5" name="password" id="password" value="">
                 <span class="mdl-textfield__error">Minumum Length 5 Characters</span>
+                <label class="mdl-textfield__label" for="sample1">Password...</label>
             </div>
             <div class="mdl-textfield mdl-js-textfield mdl-cell--6-col">
-                <input class="mdl-textfield__input" type="password" minlength="5" name="confirmPassword" id="confirmPassword" value="" placeholder="Confirm Password...">
+                <input class="mdl-textfield__input" type="password" minlength="5" name="confirmPassword" id="confirmPassword" value="">
                 <span class="mdl-textfield__error">Minumum Length 5 Characters</span>
+                <label class="mdl-textfield__label" for="sample1">Confirm Password...</label>
             </div>
             <div class="mdl-textfield mdl-js-textfield mdl-cell--6-col">
-                <input class="mdl-textfield__input" type="text" name="firstname" id="firstname" maxlength="20" value="" placeholder="First Name...">
+                <input class="mdl-textfield__input" type="text" name="firstname" id="firstname" maxlength="20" value="">
                 <span class="mdl-textfield__error"></span>
+                <label class="mdl-textfield__label" for="sample1">First Name...</label>
             </div>
             <div class="mdl-textfield mdl-js-textfield mdl-cell--6-col">
-                <input class="mdl-textfield__input" type="text" name="lastname" id="lastname" maxlength="40" value="" placeholder="Last Name...">
+                <input class="mdl-textfield__input" type="text" name="lastname" id="lastname" maxlength="40" value="">
                 <span class="mdl-textfield__error"></span>
+                <label class="mdl-textfield__label" for="sample1">Last Name...</label>
             </div>
             <div class="mdl-textfield mdl-js-textfield mdl-cell--12-col">
-                <input class="mdl-textfield__input" type="text" name="street" id="street" maxlength="40" value="" placeholder="Street...">
+                <input class="mdl-textfield__input" type="text" name="street" id="street" maxlength="40" value="">
                 <span class="mdl-textfield__error"></span>
+                <label class="mdl-textfield__label" for="sample1">Street...</label>
             </div>
             <div class="mdl-textfield mdl-js-textfield mdl-cell--4-col">
-                <input class="mdl-textfield__input" type="text" name="city" id="city" maxlength="40" value="" placeholder="City...">
+                <input class="mdl-textfield__input" type="text" name="city" id="city" maxlength="40" value="">
                 <span class="mdl-textfield__error"></span>
+                <label class="mdl-textfield__label" for="sample1">City...</label>
             </div>
             <div class="mdl-textfield mdl-js-textfield mdl-cell--4-col">
-                <input class="mdl-textfield__input" type="text" name="state" id="state" maxlength="2" value="" placeholder="State...">
+                <input class="mdl-textfield__input" type="text" name="state" id="state" maxlength="2" value="">
                 <span class="mdl-textfield__error"></span>
+                <label class="mdl-textfield__label" for="sample1">State...</label>
             </div>
             <div class="mdl-textfield mdl-js-textfield mdl-cell--4-col">
-                <input class="mdl-textfield__input" type="text" name="zip" id="zip" maxlength="5" value="" placeholder="ZIP...">
+                <input class="mdl-textfield__input" type="text" name="zip" id="zip" maxlength="5" value="">
                 <span class="mdl-textfield__error"></span>
+                <label class="mdl-textfield__label" for="sample1">Zip...</label>
             </div>
             <div class="mdl-textfield mdl-js-textfield mdl-cell--4-col">
-                <input class="mdl-textfield__input" type="text" name="birthday" id="birthday" pattern="(0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])[- /.](19|20)\d\d" placeholder="Birthday...">
+                <input class="mdl-textfield__input" type="text" name="birthday" id="birthday" pattern="(0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])[- /.](19|20)\d\d">
                 <span class="mdl-textfield__error">Format xx/xx/xxxx</span>
+                <label class="mdl-textfield__label" for="sample1">Birthday...</label>
             </div>
             <div class="mdl-textfield mdl-js-textfield mdl-cell--2-col">
 
             </div>
             <div class="mdl-textfield mdl-js-textfield mdl-cell--4-col">
-                <input class="mdl-textfield__input" type="text" maxlength="13" name="phone" id="phone" placeholder="Phone...">
+                <input class="mdl-textfield__input" type="text" maxlength="13" name="phone" id="phone">
                 <span class="mdl-textfield__error"></span>
             </div>
             <br>
