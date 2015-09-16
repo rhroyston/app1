13090ea2ddd548b8af78e7e6e9ce9b45ab01810e
Standard IP <rhroyston@gmail.com>
2015-09-16 21:46:42 +0000

    My changes

--- index.php
+++ index.php
@@ -81,8 +81,8 @@
                     <div class="mdl-logo">
                       More Information
                     </div>
-                    <ul class="mdl-mini-footer__link-list anchor">
-                      <li><a href="/help">Help</a></li>
+                    <ul class="mdl-mini-footer__link-list">
+                      <li><a class="anchor" href="/help">Help</a></li>
                       <li><a href="/terms">Privacy and Terms</a></li>
                       <li><a href="/agreement">User Agreement</a></li>
                     </ul>
