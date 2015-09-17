48a95831982e277b7506afa7b7a6d78b5f91cb50
Standard IP <rhroyston@gmail.com>
2015-09-17 02:57:24 +0000

    My changes

--- index.php
+++ index.php
@@ -97,7 +97,7 @@
                       Follow
                     </div>
                   </div>
-                  <ul class="mdl-mini-footer__link-list xl mdl-typography--text-center">
+                  <ul class=" xl mdl-typography--text-center">
                     <li><a href="https://github.com/rhroyston"><i class="fa fa-github"></i></a></li>&nbsp;&nbsp;
                     <li><a href="http://stackexchange.com/users/5863967/ron-royston"><i class="fa fa-stack-exchange"></i></a></li>&nbsp;&nbsp;
                     <li><a href="https://www.youtube.com/channel/UCrezXaWsBFPst9M2Ur3oX5w" target="_blank"><i class="fa fa-youtube-play"></i></a></li>
