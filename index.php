3d04de25ad2922490b13c177fabdda49e376bd93
Standard IP <rhroyston@gmail.com>
2015-09-17 05:22:04 +0000

    My changes

--- index.php
+++ index.php
@@ -53,14 +53,6 @@
                 <label class="mdl-textfield__label" for="search">Enter your query...</label>
               </div>
             </div>
-            <button class="mdl-button mdl-js-button mdl-js-ripple-effect mdl-button--icon" id="hdrbtn">
-              <i class="material-icons">more_vert</i>
-            </button>
-            <ul class="mdl-menu mdl-js-menu mdl-js-ripple-effect mdl-menu--bottom-right" for="hdrbtn">
-              <li class="mdl-menu__item">About</li>
-              <li class="mdl-menu__item">Contact</li>
-              <li class="mdl-menu__item">Legal information</li>
-            </ul>
           </nav>
         </div>
       </header>
