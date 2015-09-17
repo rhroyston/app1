ff309ada0d04015ce358dc0fb920061c41248313
Standard IP <rhroyston@gmail.com>
2015-09-17 03:43:43 +0000

    My changes

--- includes/head.php
+++ includes/head.php
@@ -14,7 +14,7 @@
 <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
 <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Bangers|Special+Elite' type='text/css'>
 <link rel="stylesheet" href="https://storage.googleapis.com/code.getmdl.io/1.0.0/material.min.css">
-<link rel="stylesheet" href="https://storage.googleapis.com/code.getmdl.io/1.0.4/material.grey-deep_orange.min.css" />
+<link rel="stylesheet" href="https://storage.googleapis.com/code.getmdl.io/1.0.4/material.yellow-orange.min.css" />
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
 <link rel="stylesheet" href="css/base.css">
 
