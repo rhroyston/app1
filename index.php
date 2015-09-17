a2315459b19caecdb3766febd8b98bc3cb2c4f53
Standard IP <rhroyston@gmail.com>
2015-09-17 03:55:30 +0000

    My changes

--- index.php
+++ index.php
@@ -93,14 +93,11 @@
                 ?>
                 <footer class="mdl-mini-footer">
                   <div class="mdl-mini-footer__left-section">
-                    <div class="mdl-logo">
-
-                    </div>
-                  </div>
-                  <div class=" xl mdl-typography--text-center">
-                    <a href="https://github.com/rhroyston"><i class="fa fa-github"></i></a>&nbsp;&nbsp;
-                    <a href="http://stackexchange.com/users/5863967/ron-royston"><i class="fa fa-stack-exchange"></i></a>&nbsp;&nbsp;
-                    <a href="https://www.youtube.com/channel/UCrezXaWsBFPst9M2Ur3oX5w" target="_blank"><i class="fa fa-youtube-play"></i></a>
+                    <div class="mdl-logo">www.rack.pub</div>
+                    <ul class="mdl-mini-footer__link-list">
+                      <li><a href="/help">Help</a></li>
+                      <li><a href="/terms">Privacy & Terms</a></li>
+                    </ul>
                   </div>
                 </footer>
               </div>
